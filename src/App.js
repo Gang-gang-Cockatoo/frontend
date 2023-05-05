4e984b532b9d99c70246df47c77449292a32d4ee
Alex Mitreanu <alexmitreanu04@gmail.com>
2023-05-05 20:28:20 +0300

    Test API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function App() {
-  return <div className="bg-red-500">Hello!</div>;
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      await axios
+        .get(process.env.REACT_APP_API)
+        .then(({ data }) => setApiData(data));
+    })();
+  }, []);
+
+  return <div className="bg-red-500">{apiData ? apiData.status : 'Hello'}</div>;
 }
 
 export default App;
